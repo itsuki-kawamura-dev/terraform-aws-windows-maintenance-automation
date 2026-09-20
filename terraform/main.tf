4dbafb2a4decabac936a167908651a10848abc50
itsuki-kawamura-dev <kawamura.itsuki.work@gmail.com>
2026-09-20 23:11:12 +0100

    add s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+    backend "s3" {
+    bucket = "your-terraform-state-bucket"
+    key    = "terraform-aws-windows-maintenance-automation/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
 }
 
 provider "aws" {
