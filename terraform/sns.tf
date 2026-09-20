9789bf7491fb8126392609aaaef1dee86551cc69
itsuki-kawamura-dev <kawamura.itsuki.work@gmail.com>
2026-09-21 00:46:52 +0100

    Update sns subscription

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -10,3 +10,9 @@ output "sns_topic_arn" {
   description = "SNS topic ARN for maintenance notifications"
   value       = aws_sns_topic.maintenance.arn
 }
+
+resource "aws_sns_topic_subscription" "email" {
+  topic_arn = aws_sns_topic.maintenance.arn
+  protocol  = "email"
+  endpoint  = var.notification_email
+}
